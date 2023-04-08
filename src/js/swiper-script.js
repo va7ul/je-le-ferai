ac86714deb8c9e747a38fd34dde4449e76f993cc
kohut258 <123656591+kohut258@users.noreply.github.com>
2023-04-09 00:16:38 +0200

    version-6
    не коректно працює рижим мобаїл на розмірі 320px

--- src/js/swiper-script.js
+++ src/js/swiper-script.js
@@ -1,10 +1,11 @@
 var swiper = new Swiper(".mySwiper", {
     
-  slidesPerView: 3,
-  spaceBetween: 28,
+  slidesPerView: 'auto',
+  spaceBetween: 0,
   freeMode: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    dynamicBullets: true,
   },
 });
