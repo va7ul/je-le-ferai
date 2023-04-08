bb8833fc9f5fec9978416fb15355f822ace6877f
kohut258 <123656591+kohut258@users.noreply.github.com>
2023-04-08 19:23:52 +0200

    version-3

---
+++ src/js/swiper-script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    
+  slidesPerView: 3,
+  spaceBetween: 28,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
